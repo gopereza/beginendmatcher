c8fa62eb65861929acdeb1725fa89cf141cec747
SenseyeDeveloper <senseye.com.ua@gmail.com>
2020-01-15 13:45:38 +0200

    Benchmark Matcher Match

--- matcher_test.go
+++ matcher_test.go
@@ -1,8 +1,22 @@
 package beginendmatcher
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
-func TestMatch(t *testing.T) {
+const (
+	base = 36
+
+	dataProviderShift = base * base
+	dataProviderLimit = 1 << 10
+)
+
+var (
+	dataProvider = generateDataProvider(dataProviderShift, dataProviderLimit)
+)
+
+func TestDirectMatch(t *testing.T) {
 	var test = func(expect bool, value string, prefixes []string) {
 		var matcher = NewMatcher(prefixes)
 		var got = matcher.Match(value)
@@ -16,4 +30,26 @@ func TestMatch(t *testing.T) {
 	test(false, "b", []string{"a"})
 	test(false, "abc", []string{"abcd"})
 	test(true, "abc", []string{"ab"})
+}
+
+func BenchmarkMatcher_Match(b *testing.B) {
+	var matcher = NewMatcher(dataProvider)
+
+	for i := 0; i < b.N; i++ {
+		matcher.Match(dataProvider[i%dataProviderLimit])
+	}
+}
+
+func generateDataProvider(shift, limit int) []string {
+	var (
+		to = shift + limit
+	)
+
+	var result = make([]string, 0, limit)
+
+	for i := shift; i < to; i++ {
+		result = append(result, strconv.FormatUint(uint64(i), base))
+	}
+
+	return result
 }
