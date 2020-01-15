4d8d3cc8a4e941e1be004bed13eafa3eda05ef01
SenseyeDeveloper <senseye.com.ua@gmail.com>
2020-01-15 12:02:25 +0200

    direct match, without any optimization

--- matcher.go
+++ matcher.go
@@ -1,5 +1,23 @@
 package beginendmatcher
 
-func Match(value string, prefixes []string) bool {
+import "strings"
+
+type (
+	Matcher struct {
+		values []string
+	}
+)
+
+func NewMatcher(values []string) *Matcher {
+	return &Matcher{values: values}
+}
+
+func (m *Matcher) Match(value string) bool {
+	for _, prefix := range m.values {
+		if strings.HasPrefix(value, prefix) {
+			return true
+		}
+	}
+
 	return false
 }
--- matcher_test.go
+++ matcher_test.go
@@ -3,10 +3,17 @@ package beginendmatcher
 import "testing"
 
 func TestMatch(t *testing.T) {
-	var got = Match("", []string{})
-	var expect = false
+	var test = func(expect bool, value string, prefixes []string) {
+		var matcher = NewMatcher(prefixes)
+		var got = matcher.Match(value)
 
-	if expect != got {
-		t.Errorf("expect %t, got %t", expect, got)
+		if expect != got {
+			t.Errorf("expect %t, got %t", expect, got)
+		}
 	}
+
+	test(false, "", nil)
+	test(false, "b", []string{"a"})
+	test(false, "abc", []string{"abcd"})
+	test(true, "abc", []string{"ab"})
 }
